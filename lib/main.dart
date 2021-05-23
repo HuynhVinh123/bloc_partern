c0d5851778824a147d4bd2978f29137152cd493b
HuynhVinh123 <tanvinhno1@gmail.com>
2021-05-23 14:15:27 +0700

    test rebase 4

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter dsfs sà flesjDemo Home Page'),
+      home: MyHomePage(title: 'Flutter d dsfs sà flesjDemo Home Page'),
     );
   }
 }
